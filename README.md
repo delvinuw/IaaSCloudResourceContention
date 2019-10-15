893ff6aa28a855de5ef6a3a40580a8c46ad955b9
delvinuw <delvin@uw.edu>
2019-10-14 21:47:04 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# Multi-dimensional Public Cloud Resource Contention Detection to Predict Virtual Machine Co-residency
+
+Advisor, Wes Lloyd@UWT
+Edward Han@UWT
+
+## Abstract
+
+This repo is used to purposely create Resource Contention on AWS.<br/>
+Main idea is to run a suite of benchmarks simultaneously on a Dedicated host.
+
+## How to do experiment
+
+1. provision AWS instances using **ami-0c85437aa3f534a1a**
+   - for iperf
+     - tag half instance as <key:iperfRole, val:client> and half <key:iperfRole, val:server>
+   - for pgbench
+     - provision c3 with instance store
+2. python3 distribute_work.py / iperf_master.py accordingly
+3. python3 collect_data.py
+   - for iperf
+     - use iperClient as input when collecting data
+   - for other benchmarks
+     - use hostfile input when collecting data
+4. proceeding .csv file with jupyter-notebokk accordingly, if needed
+
+## Directory Description
+
+    #scripts/
+    .
+    ├── local
+    │   ├── collect_data.py
+    │   ├── crontab # use linux crontab as a RPC interface
+    │   ├── crontab.bak
+    │   ├── distribute_work.py # entrance module on local
+    │   ├── hostfile_pssh #public IP pool
+    │   ├── iperfClients  #public IP pool
+    │   ├── iperfServers  #public IP pool
+    │   ├── iperf_master.py #for iperf only
+    └── remote
+        ├── __init__.py
+        ├── const.py #defined a sort of constant, such as benchmark name, etc.
+        ├── experiment.py #mainly defined Parser for grabing output of benchmark
+        ├── init_pg_on_localdisk.bash #will be invoked when working with pgbench
+        ├── iperf_slave.py # for iperf only
+        └── run.py # entrance module on remote
+
+## Manual
+
+run.py<br>
+
+    -c [num of cycles]
+    -i [exp_id]
+    -t [exp_type]
+
+distribute_work.py
+
+    -h #help
+    -r #dedicated host reverse_mode 1to16 (16to1 by default)
+    -t/c [minute:hour:day in UTC]/[minutes count down]
+    -n [num of works]
+    -d [dedicated host mode interval]
+
+## Updates
+
+**Oct 10. 2018** Version 1.0 - Add support to sysbench
+
+**Oct 7. 2018** Add two sepretated .py for iperf experiment
+
+     iperf_master.py
+     iperf_slave.py
+
+- Dependencies
+
+  - slavenode
+
+    - git, all benchmarks [iperf, pgbench, y-cruncher, sysbench, ...]
+
+  - masternode
+
+    - pssh, scp, ssh
+
+  - shared
+    - python3 [re,csv...a sort of packages]
+    - environment variables
+    - file paths [.pem .csv .py]
+
+Previous module have some design flaw since most of the work was served for making a proof that our method is feasible<br>
+For convenience and agile, I simplely mixed python3 and some bash scripts without encapsulation, which lead to a painful experience of maintaining after that...<br>
+At this time, rewrite is much faster than reuse... well, quality of those two new added module is quite better than what I wrote before.
+Overall, it's fairly scalable now, and definitely can achieve our goal :)<br>
+I'm on the way to coding elegantly!
+
+**July 22. 2018** Add a init script for pgbench experiment
+
+     init_pg_on_localdisk.bash
---
+++ scripts/remote/experiment.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+run.py a experiment.py  with options; 
+data should be collect every run, otherwise it will be overwrite;
+'''
+
+import re
+import csv
+import const
+import os
+import time
+from collections import OrderedDict
+
+
+class parser(object):
+    # kw need experimentID testOption
+    def __init__(self, benchmark, string, **kw):
+        self.string = string.split('\n')
+        self.benchmark = benchmark
+        self.kw = kw
+        ec2metadata = os.popen('ec2metadata').read()
+        self.kw['instanceType'] = re.search(
+            r'instance-type: (\w*\.\w*)', ec2metadata).group(1)
+        self.kw['instanceID'] = re.search(
+            r'instance-id: (.*)\n', ec2metadata).group(1)
+
+    def y_cruncher(self):
+        needHeader = False
+        if not os.path.isfile(const.datadir+'y_cruncher.csv'):
+            needHeader = True
+        os.system("mkdir "+const.datadir)
+        with open(const.datadir+'y_cruncher.csv', 'a') as fout:
+            row = OrderedDict([('instanceID', None), ('experimentID', None), ('instanceType', None),
+                               ('memoryInfo', None), ('processorInfo',
+                                                      None), ('sysTopology', None),
+                               ('osVersion', None), ('testStartTime',
+                                                     None), ('availableMemory', None),
+                               ('isMultiThread', None), ('cpuUtilization',
+                                                         None), ('multiCoreEfficiency', None),
+                               ('computationTime', None), ('benchmarkTime',
+                                                           None), ('wallTime', None)
+                               ])
+            # benchmarkTime = computationTime + I/O operation overhead
+            writer = csv.DictWriter(fout, fieldnames=row)
+            if needHeader:
+                writer.writeheader()
+            row['instanceType'] = self.kw['instanceType']
+            row['instanceID'] = self.kw['instanceID']
+            row['experimentID'] = self.kw['experimentID']
+            row['wallTime'] = self.kw['duration']
+            # row['testOption']=self.kw['testOption']
+
+            for line in self.string:
+
+                if line.find('Multi-core Efficiency') != -1:
+                    obj = re.search(r'(\d*\.\d* %)', line)
+                    row['multiCoreEfficiency'] = obj.group(1)
+                if line.find('CPU Utilization') != -1:
+                    obj = re.search(r'(\d*\.\d* %)', line)
+                    row['cpuUtilization'] = obj.group(1)
+                if line.find('Multi-Threading') != -1:
+                    obj = re.search(r'\[01;36m(\w*)', line)
+                    row['isMultiThread'] = obj.group(1)
+                if line.find('Available Memory') != -1:
+                    obj = re.search(r'1;33m(.*?B)', line)
+                    row['availableMemory'] = obj.group(1)
+                if line.find('Version') != -1:
+                    obj = re.search(r'(\s+)(.*)', line)
+                    row['osVersion'] = obj.group(2)
+                if line.find('Topology') != -1:
+                    obj = re.search(r'(\s+)(.*)', line)
+                    row['sysTopology'] = obj.group(2)
+                if line.find('Processor(s):') != -1:
+                    obj = re.search(r'(\s+)(.*)', line)
+                    row['processorInfo'] = obj.group(2)
+                if line.find('Usable Memory') != -1:
+                    obj = re.search(r'\((.*?B)', line)
+                    row['memoryInfo'] = obj.group(1)
+                if line.find('Start Time') != -1:
+                    obj = re.search(
+                        r'Start Time: .*?(01;33m)(.*)(\[01;37m)', line)
+                    row['testStartTime'] = obj.group(2)
+                if line.find('Wall Time') != -1:
+                    obj = re.search(r'(\d*\.\d*).*seconds', line)
+                    row['benchmarkTime'] = obj.group(1)
+                if line.find('Total Computation') != -1:
+                    obj = re.search(r'(\d*\.\d*).*seconds', line)
+                    row['computationTime'] = obj.group(1)
+                # TODO more attributes
+
+            writer.writerow(row)
+
+    def pgbench(self):
+        needHeader = False
+        if not os.path.isfile(const.datadir+'pgbench.csv'):
+            needHeader = True
+        os.system("mkdir "+const.datadir)
+        with open(const.datadir+'pgbench.csv', 'a') as fout:
+            row = OrderedDict([('instanceID', None), ('experimentID', None), ('instanceType', None), ('wallTime', None),
+                               ('clients', None), ('threads', None), ('scaleFactor',
+                                                                      None), ('transactionsType', None),
+                               ('queryMode', None), ('duration',
+                                                     None), ('transactions', None), ('mountPoint', None)
+                               ])
+            writer = csv.DictWriter(fout, fieldnames=row)
+            if needHeader:
+                writer.writeheader()
+            row['instanceType'] = self.kw['instanceType']
+            row['instanceID'] = self.kw['instanceID']
+            row['experimentID'] = self.kw['experimentID']
+            row['wallTime'] = self.kw['duration']
+
+                #'sed -n "s/^data_directory/data_directory/p" /etc/postgresql/9.5/main/postgresql.conf | cut -d\'#\' -f 1').readline().rstrip().partition('\n')[0]
+            mountPoint = os.popen(
+                'sed -n "s/^data_directory/data_directory/p" /etc/postgresql/9.5/main/postgresql.conf | cut -d\'#\' -f 1').readline().rstrip()
+            row['mountPoint'] = mountPoint
+            for line in self.string:
+                if line.find('clients:') != -1:
+                    obj = re.search(r'(\d+)', line)
+                    row['clients'] = obj.group(1)
+                if line.find('threads:') != -1:
+                    obj = re.search(r'(\d+)', line)
+                    row['threads'] = obj.group(1)
+                if line.find('factor:') != -1:
+                    obj = re.search(r'(\d+)', line)
+                    row['scaleFactor'] = obj.group(1)
+                if line.find('type:') != -1:
+                    obj = re.search(r': (.*)', line)
+                    row['transactionsType'] = obj.group(1)
+                if line.find('mode:') != -1:
+                    obj = re.search(r': (.*)', line)
+                    row['queryMode'] = obj.group(1)
+                if line.find('duration:') != -1:
+                    obj = re.search(r': (.*)', line)
+                    row['duration'] = obj.group(1)
+                if line.find('processed:') != -1:
+                    obj = re.search(r'(\d+)', line)
+                    row['transactions'] = obj.group(1)
+            writer.writerow(row)
+        pass
+
+    def sysbench(self):
+        needHeader = False
+        if not os.path.isfile(const.datadir + 'sysbench.csv'):
+            needHeader = True
+        os.system("mkdir " + const.datadir)
+        with open(const.datadir+'sysbench.csv', 'a') as fout:
+            row = OrderedDict([('experimentID', None), ('instanceID', None), ('instanceType', None),
+                               ('wallTime', None), ('testOption',
+                                                    None), ('per-request-avg-time', None),
+                               ('total-time', None), ('thread-num', None)
+                               ])
+
+            writer = csv.DictWriter(fout, fieldnames=row)
+            if needHeader:
+                writer.writeheader()
+            row['instanceType'] = self.kw['instanceType']
+            row['instanceID'] = self.kw['instanceID']
+            row['experimentID'] = self.kw['experimentID']
+            row['wallTime'] = self.kw['duration']
+            row['testOption'] = self.kw['testOption']
+
+            i = 0
+            for line in self.string:
+                if line.find('per-request statistics:') != -1:
+                    target = self.string[i + 2]
+                    avg = re.search(r'avg:\s*(.*)', target).group(1)
+                    row['per-request-avg-time'] = avg
+                if line.find('total time:') != -1:
+                    totalTime = re.search(r'total time:\s*(.*)', line).group(1)
+                    row['total-time'] = totalTime
+                if line.find('Number of threads:') != -1:
+                    threadNum = re.search(
+                        r'Number of threads:\s*(.*)', line).group(1)
+                    row['thread-num'] = threadNum
+                i += 1
+
+            writer.writerow(row)
+
+    def sysbench_ram(self):
+        needHeader = False
+        if not os.path.isfile(const.datadir + 'sysbench_ram.csv'):
+            needHeader = True
+        os.system("mkdir " + const.datadir)
+        with open(const.datadir+'sysbench_ram.csv', 'a') as fout:
+            row = OrderedDict([('experimentID', None), ('instanceID', None), ('instanceType', None),
+                               ('wallTime', None), ('testOption',None), ('ram_write_speed', None),
+                               ('total-time', None), ('thread-num', None)
+                               ])
+
+            writer = csv.DictWriter(fout, fieldnames=row)
+            if needHeader:
+                writer.writeheader()
+            row['instanceType'] = self.kw['instanceType']
+            row['instanceID'] = self.kw['instanceID']
+            row['experimentID'] = self.kw['experimentID']
+            row['wallTime'] = self.kw['duration']
+            row['testOption'] = self.kw['testOption']
+
+            i = 0
+            for line in self.string:
+                if line.find('transferred') != -1:
+                    speed = re.search(r'\((\d*\.\d*) MB/sec\)', line).group(1)
+                    row['ram_write_speed'] = speed
+                if line.find('total time:') != -1:
+                    totalTime = re.search(r'total time:\s*(.*)', line).group(1)
+                    row['total-time'] = totalTime
+                if line.find('Number of threads:') != -1:
+                    threadNum = re.search(
+                        r'Number of threads:\s*(.*)', line).group(1)
+                    row['thread-num'] = threadNum
+                i += 1
+
+            writer.writerow(row)
+    def stress_ng(self):
+        pass
+
+    def bonnie(self):
+        pass
+
+    def iperf3(self):
+        pass
+
+    def bandwidth(self):
+        pass
+
+    def mbw(self):
+        pass
+
+    y_cruncherc3 = y_cruncher
+    y_cruncherc4 = y_cruncher
+    y_cruncherz1d = y_cruncher
+    y_cruncherm5d = y_cruncher
+	 
+    def getfunc(self):
+        return getattr(self, self.benchmark)
+
+
+class Experiment(object):
+    def __init__(self, benchmark, cycle, options, experimentID, phantomIdle):
+        self.benchmark = benchmark
+        self.cycle = int(cycle)
+        self.options = options
+        self.experimentID = experimentID
+        self.phontomIdle = phantomIdle
+
+    def run(self):
+        for i in range(self.cycle):
+            # flush cache
+            os.popen("echo 3 | sudo tee /proc/sys/vm/drop_caches").read()
+            print(const.command[self.benchmark]+self.options[self.benchmark])
+            
+            # time stamp that user percieved
+            time1 = time.time()
+           
+            if (self.pahtntomIdle < 0): 
+                #phantomIdle default to -1 so run normal cascading experiments
+                result = os.popen(
+                    const.command[self.benchmark]+self.options[self.benchmark]).read()
+            elif (i == 0): 
+                #run single vm and rest idle
+                result = os.popen(
+                    const.command[self.benchmark]+self.options[self.benchmark]).read()
+            else: 
+                #wait for phantomIdle seconds
+                time.sleep(self.phantomIdle)
+                
+            time2 = time.time()
+            duration = time2-time1  # unit in seconds
+            
+            myParser = parser(self.benchmark, result, testOption=self.options[self.benchmark],
+                              duration=duration, experimentID=self.experimentID)
+            func = myParser.getfunc()
+            func()
+            print(result)
