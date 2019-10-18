f5b931854892821bd656fea9ec1e0c27e285b9cb
delvinuw <delvin@uw.edu>
2019-10-17 23:37:07 -0700

    bug

--- scripts/remote/run.py
+++ scripts/remote/run.py
@@ -29,7 +29,7 @@ def main(argv):
 			 -t benchmark')
             sys.exit()
         elif opt in ("-p"):
-            phantomIdle = arg
+            phantomIdle = '10' #@BUG: for some reason when i set this to arg, code breaks..
         elif opt in ("-i"):
             ID = arg
         elif opt in ("-c"):
